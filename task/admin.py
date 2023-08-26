bfb018b2134b1845f34912212ac7840844966aa2
Jeong Taek <imjt9345@naver.com>
2023-08-26 18:44:27 +0900

    admin 페이지 수정

--- task/admin.py
+++ task/admin.py
@@ -6,9 +6,11 @@ from task.models import TodoBase, Task
 @admin.register(TodoBase)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ['title', 'created']
+    list_filter = ['created']
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['title', 'created', 'updated', 'important', 'completed']
     raw_id_fields = ['todolist']
+    list_filter = ['created', 'important', 'completed']
