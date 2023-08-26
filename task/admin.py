8430e590234f50e3b2661cd29ca07eaf583d6c50
Jeong Taek <imjt9345@naver.com>
2023-08-26 18:08:03 +0900

    admin 페이지 추가

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from task.models import TodoBase, Task
+
+
+@admin.register(TodoBase)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created', 'updated', 'important', 'completed']
+    raw_id_fields = ['todolist']
