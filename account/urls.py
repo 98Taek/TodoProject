aeadc825d2e6e74b7f325fa09ee2ab7fcf4c3a1f
Jeong Taek <imjt9345@naver.com>
2023-08-27 20:41:05 +0900

    login, logout, register 뷰 함수 템플릿 추가

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+from account import views
+
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
