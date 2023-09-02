1f1ee80d10f92534b154fec783a8afed3ff1ffdb
Jeong Taek <imjt9345@naver.com>
2023-09-02 23:21:49 +0900

    CustomPasswordChangeDone 뷰 추가

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from account import views
 
 urlpatterns = [
+    path('password_change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.RegisterView.as_view(), name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeDoneView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -10,6 +11,14 @@ class CustomLoginView(LoginView):
         if request.user.is_authenticated:
             redirect('task:home')
         return super().dispatch(request, *args, **kwargs)
+
+
+class CustomPasswordChangeDoneView(PasswordChangeDoneView):
+    template_name = 'registration/password_change_done.html'
+
+    def get(self, request, *args, **kwargs):
+        logout(self.request)
+        return super().get(request, *args, **kwargs)
 
 
 class RegisterView(CreateView):
