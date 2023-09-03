f27701c1c5f3d1ecd8a0438e9f7a00b8845ff17d
Jeong Taek <imjt9345@naver.com>
2023-09-03 16:25:04 +0900

    기본 페이지 -> 로그인 페이지로 설정

--- todo/urls.py
+++ todo/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from account.views import CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 
+    path('', CustomLoginView.as_view(), name='login'),
     path('task/', include('task.urls')),
 ]
