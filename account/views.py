4cd30ea0a61cb2a0d86901181176a4537ff941b8
Jeong Taek <imjt9345@naver.com>
2023-09-03 17:25:13 +0900

    login.html 스타일 입히기

--- account/views.py
+++ account/views.py
@@ -5,8 +5,12 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from account.forms import CustomLoginForm
+
 
 class CustomLoginView(LoginView):
+    form_class = CustomLoginForm
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             redirect('task:home')
